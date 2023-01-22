e992e4351311fa6ed44321920bc5f962f2922fab
MarioMotario <mix-777@ukr.net>
2023-01-22 16:01:48 +0100

    home work 4 done

--- src/com/ua/robot/homework3/HMsecondlesson.java
+++ src/com/ua/robot/homework3/HMsecondlesson.java
@@ -1,6 +1,6 @@
 package com.ua.robot.homework3;
 
-public class SecondLesson {
+public class HMsecondlesson {
 
     public static void main(String[] args) {
         System.out.println("Введемо дві змінні та виконаємо з ними дії");
---
+++ src/com/ua/robot/homework4/HMString.java
@@ -0,0 +1,25 @@
+package com.ua.robot.homework4;
+
+public class HMString {
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println(str);
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+
+        System.out.println(str.charAt(0));
+
+        int lenght = str.length();
+        int lastIndex = lenght - 1;
+
+        System.out.println(str.charAt(lastIndex));
+
+
+
+
+
+    }
+}
