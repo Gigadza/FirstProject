5ddafa2c9245f5934d0bec1b6ead44e485fcc31c
MarioMotario <mix-777@ukr.net>
2023-01-22 15:37:06 +0100

    home work done

---
+++ src/com/ua/robot/homework3/SecondLesson.java
@@ -0,0 +1,14 @@
+package com.ua.robot.homework3;
+
+public class SecondLesson {
+
+    public static void main(String[] args) {
+        System.out.println("Введемо дві змінні та виконаємо з ними дії");
+        int a = 100;
+        int b = 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
